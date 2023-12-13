1a75d340f8f1fb39fe063c0d909732ae2da87cd4
samphillips123 <sphillips123@gmail.com>
2023-12-13 15:33:04 -0800

    updated comments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -116,7 +116,6 @@ const Nav = () => {
                     <>
                         <Link to='home' duration={0} smooth={true} className='cursor-pointer' >
                             <div className='flex items-center justify-center' >
-                                {/* <img src={NSLogo} alt='NeuroSynth Logo' className='w-12' /> */}
                                 <div className='flex justify-center -mx-2 leading-4'>
                                     <h1 className='font-black text-primary-green'>Samuel</h1>
                                     <h1 className='font-normal'>Phillips</h1>
@@ -165,7 +164,6 @@ const Nav = () => {
                             onClick={() => setToggled(false)} // this onClick closes the nav if the home link is clicked"
                         >
                             <div className='flex items-center justify-center' >
-                                {/* <img src={NSLogo} alt='NeuroSynth Logo' className='w-12' /> */}
                                 <div className='flex justify-center -mx-2 leading-4'>
                                     <h1 className='font-black text-primary-green'>Samuel</h1>
                                     <h1 className='font-normal'>Phillips</h1>
